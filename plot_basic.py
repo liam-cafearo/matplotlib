cbfbc1bdb5301735a64e85ee26fa238956a2f36f
Liam-Cafearo <lcafearo@gmail.com>
2018-05-16 22:39:03 +0100

    Added comments to describe what is happening in the code

--- plot_basic.py
+++ plot_basic.py
@@ -3,8 +3,10 @@
 # standard for python programs
 import matplotlib.pyplot as plt
 
-# this takes the '.plot' method of 
-# plypot to plot some coordinates
+# this takes the '.plot' method of plypot to plot some coordinates.
+# '.plot' can take many parameters but first two are x and y coordinates
+# which we have placed into lists meaning we have three 
+# coordinates '1,5' '2,7' '3,4
 plt.plot([1,2,3],[5,7,4])
 
 # '.plot' draws the plot in the 
